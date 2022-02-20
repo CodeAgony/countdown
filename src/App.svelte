b315fd7adba3d5c40a89d524bede4a191d07308f
CodeAgony <yaroslav.bkh@gmail.com>
2022-02-21 00:31:47 +0200

    Add time diff

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
+	export let diff;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{diff} days left till the big day</h1>
 </main>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import App from './App.svelte';
+import { differenceInDays } from 'date-fns';
+
+const date = new Date()
+const finalDate = new Date(1648072800 * 1000)
+const diff = differenceInDays(finalDate, date)
+
 
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		diff
 	}
 });
 
